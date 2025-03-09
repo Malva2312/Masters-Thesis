475153b9143b726ad6d8c76bf4c0f79e7628fba2
João Malva <up202006605@edu.fe.up.pt>
2025-03-09 23:54:39 +0100

    Add test_step method to LungNoduleClassifier for model evaluation

--- src/LungNoduleClassifier.py
+++ src/LungNoduleClassifier.py
@@ -30,7 +30,7 @@ class LungNoduleClassifier(pl.LightningModule):
         self.log("train_loss", loss, prog_bar=True)
         self.log("train_acc", acc, prog_bar=True)
         return loss
-
+    
     def validation_step(self, batch, batch_idx):
         images, labels = batch
         labels = labels.float().unsqueeze(1)
@@ -41,6 +41,31 @@ class LungNoduleClassifier(pl.LightningModule):
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_acc", acc, prog_bar=True)
         return loss
+    
+    def test_step(self, batch, batch_idx):
+        """
+        Perform a single test step.
+
+        Args:
+            batch (tuple): A tuple containing the input images and corresponding labels.
+            batch_idx (int): The index of the batch.
+
+        This method performs the following steps:
+        1. Extracts images and labels from the batch.
+        2. Converts labels to float and adds an extra dimension.
+        3. Passes the images through the model to obtain logits.
+        4. Computes the loss between the logits and the labels.
+        5. Calculates the predictions by applying a sigmoid function and rounding the results.
+        6. Computes the accuracy by comparing the predictions with the labels.
+        7. Logs the test accuracy.
+        """
+        images, labels = batch
+        labels = labels.float().unsqueeze(1)
+        logits = self(images)
+        loss = self.loss_fn(logits, labels)
+        preds = torch.sigmoid(logits).round()
+        acc = (preds == labels).float().mean()
+        self.log("test_acc", acc, prog_bar=True) #And other metrics
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.lr)

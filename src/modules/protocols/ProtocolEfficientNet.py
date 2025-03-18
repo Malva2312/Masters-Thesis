3a2ea6b386dc3fca0fb32d56ffefc0c425b2d267
João Malva <up202006605@edu.fe.up.pt>
2025-03-18 11:13:06 +0000

    Refactor ProtocolEfficientNet to support variable input channels and adjust convolutional layer parameters

--- src/modules/protocols/ProtocolEfficientNet.py
+++ src/modules/protocols/ProtocolEfficientNet.py
@@ -4,10 +4,18 @@ import torch
 from torch import optim, nn
 
 class ProtocolEfficientNet(pl.LightningModule):
-    def __init__(self, num_classes, model_name='efficientnet-b0'):
+    def __init__(self, num_classes, num_channels=3, model_name='efficientnet-b0'):
         super().__init__()
         self.save_hyperparameters()
         self.model = EfficientNet.from_pretrained(model_name)
+        
+        # Modify the first convolutional layer to accept the specified number of input channels
+        self.model._conv_stem = nn.Conv2d(num_channels, self.model._conv_stem.out_channels, 
+                                          kernel_size=3,  # Adjusted kernel size
+                                          stride=self.model._conv_stem.stride, 
+                                          padding=1,  # Adjusted padding
+                                          bias=False)
+        
         self.model._fc = nn.Linear(self.model._fc.in_features, num_classes)
         self.loss_fn = nn.BCEWithLogitsLoss()
 
@@ -16,6 +24,10 @@ class ProtocolEfficientNet(pl.LightningModule):
     
     def step(self, batch):
         images, labels = batch
+
+        images = images['input_image']
+        # images = images.view(images.size(0), -1)
+
         labels = labels['lnm']['mean'].float().unsqueeze(1)
         logits = self(images)
         loss = self.loss_fn(logits, labels)
@@ -25,7 +37,6 @@ class ProtocolEfficientNet(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss, acc = self.step(batch)
-        
         self.log("train_loss", loss, prog_bar=True)
         self.log("train_acc", acc, prog_bar=True)
         return loss

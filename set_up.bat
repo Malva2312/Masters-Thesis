114a6868b6c70de4628a84d53982b329a7f2bf4a
João Malva <up202006605@edu.fe.up.pt>
2025-03-24 17:26:12 +0000

    Enhance setup script to remove existing data directories before unzipping and copy data to the root directory

--- set_up.bat
+++ set_up.bat
@@ -7,8 +7,22 @@ set ZIP_FILE_PATH=.\src\data_loading.zip
 REM Set the destination directory
 set DEST_DIR=.\src
 
+REM Check if the destination directory exists and remove it if it does
+if exist %DEST_DIR%\data_loading (
+    echo Removing existing directory %DEST_DIR%\data_loading
+    rmdir /s /q %DEST_DIR%\data_loading
+)
+
 REM Unzip the file
 tar -xf %ZIP_FILE_PATH% -C %DEST_DIR%
 
 echo Unzipping completed.
-pause
+
+REM Check if the destination directory already exists and remove it if it does
+if exist .\data (
+    echo Removing existing directory .\data
+    rmdir /s /q .\data
+)
+
+REM Copy data from .\src\data_loading\data to .\
+xcopy .\src\data_loading\data .\data\ /s /e /i

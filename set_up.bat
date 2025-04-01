3a1826fc15faf00238970b03f5b520cd661e8b16
João Malva <up202006605@edu.fe.up.pt>
2025-04-01 10:21:27 +0100

    Refactor setup script to streamline conda environment setup process

--- set_up.bat
+++ set_up.bat
@@ -27,14 +27,7 @@ if exist .\data (
 REM Copy data from .\src\data_loading\data to .\
 xcopy .\src\data_loading\data .\data\ /s /e /i
 
+REM Call conda.bat to set up the environment
+call .\conda.bat
 
-REM Create the conda environment if it doesn't already exist
-conda env list | find "lung_char_fusion_env" >nul 2>&1
-if %errorlevel% == 0 (
-    echo Conda environment already exists. Skipping creation.
-) else (
-    echo Creating conda environment
-    conda env create -f .\src\conda_env\conda_env.yml
-)
-
-echo Conda environment setup completed.
+echo Setup completed.

a6f5dd3c21597113213015bf926e0450b9e8a8df
João Malva <up202006605@edu.fe.up.pt>
2025-04-02 23:39:24 +0100

    Enhance MainApplication to accept command-line arguments for configuration and protocol settings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import hydra
 from modules.Manager import DataLoaderManager, TrainerManager, DataInfo
+import argparse
+import yaml
 
 class MainApplication:
-    def __init__(self):
+    def __init__(self, args):
         hydra.initialize(config_path='./config', version_base=None)
         self.config = hydra.compose(
             config_name="config", 
@@ -12,7 +14,12 @@ class MainApplication:
             ]
         )
         self.dataloader_manager = DataLoaderManager(self.config)
-        self.trainer_manager = TrainerManager(self.config, self.dataloader_manager)
+        self.trainer_manager = TrainerManager(
+            self.config, 
+            self.dataloader_manager, 
+            protocol=args.protocol, 
+            protocol_params=args.protocol_params
+        )
 
     def run(self):
         print("\n-------------------------------------- Demonstrating the K-fold data loader ---------------------------------------\n")
@@ -32,5 +39,15 @@ class MainApplication:
         self.trainer_manager.test_model()
 
 if __name__ == "__main__":
-    app = MainApplication()
+    parser = argparse.ArgumentParser(description="Main application for training lung nodule classifiers.")
+    parser.add_argument("--config_file", type=str, required=True, help="Path to the YAML configuration file.")
+    args = parser.parse_args()
+
+    with open(args.config_file, 'r') as file:
+        config_data = yaml.safe_load(file)
+
+    args.protocol = config_data.get("protocol", None)
+    args.protocol_params = config_data.get("protocol_params", {})
+
+    app = MainApplication(args)
     app.run()
--- src/modules/Manager.py
+++ src/modules/Manager.py
@@ -9,7 +9,7 @@ from data_loading.src.modules.data.dataloader.preprocessed_data_loader import LI
 from data_loading.src.modules.data.metadata import LIDCIDRIPreprocessedMetaData
 from data_loading.src.modules.utils.paths import PYTHON_PROJECT_DIR_PATH
 
-from modules.LungNoduleClassifier import LungNoduleClassifier
+# from modules.LungNoduleClassifier import LungNoduleClassifier # Example protoco 
 from modules.protocols.ProtocolEfficientNet import ProtocolEfficientNet
 from modules.protocols.ProtocolRadiomics import ProtocolRadiomics
 
@@ -38,27 +38,36 @@ class DataLoaderManager:
 
     def print_loaded_data_info(self, data_info, k_fold_data_loaders=False, load_mask=False):
         space = "    " if k_fold_data_loaders else ""
-        print(f"{space}    Batch index: {data_info.batch_index}")
-        print(f"{space}        Data (Lung nodule CT image):")
-        print(f"{space}         - Type: {type(data_info.data['input_image']).__name__}")
-        print(f"{space}         - Shape: {data_info.data['input_image'].shape}")
-        print(f"{space}         - Min/max values: {data_info.data['input_image'].min()}/{data_info.data['input_image'].max()}")
-        print(f"{space}        Label (Mean lung nodule malignancy)")
-        print(f"{space}         - Type: {type(data_info.label['lnm']['mean']).__name__}")
-        print(f"{space}         - Shape: {data_info.label['lnm']['mean'].shape}")
-        print(f"{space}         - Min/max values: {data_info.label['lnm']['mean'].min()}/{data_info.label['lnm']['mean'].max()}")
+        # print(f"{space}    Batch index: {data_info.batch_index}")
+        # print(f"{space}        Data (Lung nodule CT image):")
+        # print(f"{space}         - Type: {type(data_info.data['input_image']).__name__}")
+        # print(f"{space}         - Shape: {data_info.data['input_image'].shape}")
+        # print(f"{space}         - Min/max values: {data_info.data['input_image'].min()}/{data_info.data['input_image'].max()}")
+        # print(f"{space}        Label (Mean lung nodule malignancy)")
+        # print(f"{space}         - Type: {type(data_info.label['lnm']['mean']).__name__}")
+        # print(f"{space}         - Shape: {data_info.label['lnm']['mean'].shape}")
+        # print(f"{space}         - Min/max values: {data_info.label['lnm']['mean'].min()}/{data_info.label['lnm']['mean'].max()}")
 
 class TrainerManager:
-    def __init__(self, config, dataloader_manager):
+    def __init__(self, config, dataloader_manager, protocol, protocol_params):
         self.config = config
         self.dataloader_manager = dataloader_manager
+        self.protocol = protocol
+        self.protocol_params = protocol_params
         self.trainer = Trainer(limit_train_batches=100, max_epochs=1)
         self.autoencoder = None
 
+        print(f"Protocol: {self.protocol}")
+        print(f"Protocol parameters: {self.protocol_params}")
+
+
     def setup_model(self):
-        #self.autoencoder = LungNoduleClassifier()
-        #self.autoencoder = ProtocolEfficientNet(num_classes=1, num_channels=1)
-        self.autoencoder = ProtocolRadiomics(num_classes=1, num_channels=1)
+        if self.protocol == "ProtocolEfficientNet":
+            self.autoencoder = ProtocolEfficientNet(**self.protocol_params)
+        elif self.protocol == "ProtocolRadiomics":
+            self.autoencoder = ProtocolRadiomics(**self.protocol_params)
+        else:
+            raise ValueError(f"Unknown protocol: {self.protocol}")
 
     def train_model(self):
         self.trainer.fit(model=self.autoencoder, train_dataloaders=self.dataloader_manager.get_data_loaders_by_subset()["train"][0])

8699ae71792a73100cebaa667bb7f0780b4dcbc1
João Malva <up202006605@edu.fe.up.pt>
2025-04-16 13:25:27 +0100

    Add quality of service QoS that was missing on .sh slurm files

--- slurm_files/shell_script_files/run_experiment_pipeline_job.sh
+++ slurm_files/shell_script_files/run_experiment_pipeline_job.sh
@@ -3,6 +3,7 @@
 # Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
 #SBATCH --job-name=test_net
 #SBATCH --partition=gpu_min8gb
+#SBATCH --qos=gpu_min8gb_ext
 #SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
 #SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
 

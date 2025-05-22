b5b6b84ba68e70cb1dcc97c59a0dffc565589b6b
João Malva <jmalva@ctm-login-02.inesctec.pt>
2025-05-22 10:38:34 +0100

    Added New feature extractors - project skeleton, not implemented yet

---
+++ src/modules/features/gradient/blob.py
@@ -0,0 +1,8 @@
+'''
+Not essential if you already have precise nodule localization,
+but can provide complementary information.
+Consider extracting blobness-related features
+(e.g., using skimage.feature.blob_log) as part of your feature set,
+but do not rely solely on them for characterization.
+'''
+# log, dog, doh

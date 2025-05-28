b1b82428a15a4a9e0a2dab446cd824855b31fb55
João Malva <jmalva@ctm-login-01.inesctec.pt>
2025-05-28 09:32:08 +0100

    Add experiment 10 and configuration files

---
+++ experiment_results/experiment_10/experiment_info.md
@@ -0,0 +1,4 @@
+Experiment ID: 10
+Who ran it: Joao Malva
+When was it run: 2025-05-27 19:41
+What was run: Linear SVM - Testing Feature Extrator Manager
---
+++ slurm_files/shell_script_files/default_run_experiment_pipeline_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
+#SBATCH --job-name=exp_models
+
+#SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
+#SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
+
+# Command-line to run the script for dataset generation.
+srun python3 ../../src/scripts/run_experiment_pipeline.py 
+
